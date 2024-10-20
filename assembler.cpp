62910ef78aa964731633fe71b6ad9cd0a1f46f3d
Sergey Slom <slomser@mail.ru>
2024-10-20 19:33:00 +0300

    second commit

--- assembler.cpp
+++ assembler.cpp
@@ -1,6 +1,55 @@
 #include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
 
-int assembler(FILE* commands)
+#include "assembler.h"
+
+int read_user_code(char* cmd);
+
+int assembler(code_info* data, FILE* code_data, stack* stk, Register* reg)
 {
-    
+    while(1)
+    {
+        char cmd[1000];
+
+        fscanf(code_data, cmd);
+
+        switch(read_user_code(cmd))
+        {
+            case HLT: break;
+            
+            case PUSH:  {
+                            int arg = 0;
+
+                            fscanf(code_data, (const char*) arg)
+
+                            stack_push(stk, arg);
+                        }
+            case POP:   {
+                            reg->reg_array[reg->reg_elem_num] = stack_pop(stk);
+
+                            (reg->reg_elem_num)++;
+                        }
+            
+            default:
+                break;
+        }
+    }
+}
+
+int read_user_code(char* cmd)
+{
+    if(strcmp(cmd, "hlt")) return HLT;
+    if(strcmp(cmd, "push")) return PUSH;
+    if(strcmp(cmd, "pop")) return POP;
+    if(strcmp(cmd, "sub")) return SUB;
+    if(strcmp(cmd, "add")) return ADD;
+    if(strcmp(cmd, "mul")) return MUL;
+    if(strcmp(cmd, "div")) return DIV;
+    if(strcmp(cmd, "out")) return OUT;
+    if(strcmp(cmd, "in")) return IN;
+    if(strcmp(cmd, "sqrt")) return SQRT;
+    if(strcmp(cmd, "sin")) return SIN;
+    if(strcmp(cmd, "cos")) return COS;
+    if(strcmp(cmd, "dump")) return DUMP;
 }
--- assembler.h
+++ assembler.h
@@ -3,6 +3,58 @@
 
 #include <stdio.h>
 
-int assembler(FILE* commands);
+struct code_info
+{
+    int* code;
+    size_t code_size;
+    
+    size_t funcsion_has_arg;
+    int current_function;
+};
 
+struct Register
+{
+    int* reg_array;
+
+    size_t reg_elem_num;
+};
+
+enum cmd
+{
+    HLT  = -1,
+    PUSH =  1,
+    POP  =  2,
+    SUB  =  3,
+    ADD  =  4,
+    MUL  =  5,
+    DIV  =  6,
+    OUT  =  7,
+    IN   =  8,
+    SQRT =  9,
+    SIN  = 10,
+    COS  = 11,
+    DUMP = 12
+};
+
+// struct cmd
+// {
+//     int hlt  = HLT;
+//     int push = PUSH;
+//     int pop  = POP;
+//     int sub  = SUB;
+//     int add  = ADD;
+//     int mul  = MUL;
+//     int div  = DIV;
+//     int out  = OUT;
+//     int in   = IN;  //vvod+push
+//     int sqrt = SQRT;
+//     int sin  = SIN;
+//     int cos  = COS;
+//     int dump = DUMP;
+// };
+
+#define file_name "code.txt"
+
+
+int assembler(code_info* data, FILE* code_data, stack* stk, Register* reg);
 #endif
